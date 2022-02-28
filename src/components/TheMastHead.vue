89255de122cf3b02043f00c94244ab1dd3953443
Amina N Oti <Amina.Oti@ibm.com>
2022-02-28 16:53:37 +0000

    feat: add TheMastHead

---
+++ src/components/TheMastHead.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header-bar">
+    <div style="left: 80px"></div>
+    <H1> Header </H1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TheMasthead",
+};
+</script>
+
+<style>
+.header-bar {
+  color: white;
+  background-color: black;
+  padding: 1%;
+}
+h1 {
+  right: 100xp;
+}
+</style>
