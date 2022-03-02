93d53c5c619987b349914ec2672f684d897c9a3a
Amina N Oti <Amina.Oti@ibm.com>
2022-03-02 09:22:16 +0000

    feat: test for TheMastHead.vue

---
+++ tests/unit/components/TheMastHead.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from "@vue/test-utils";
+import TheMastHead from "@/components/TheMastHead.vue";
+
+describe("The header - TheMastHead.vue", () => {
+  it("should render the title passed through the props", () => {
+    const siteTitle = "Mock Site Title";
+    const wrapper = shallowMount(TheMastHead, {
+      props: { siteTitle },
+    });
+    expect(wrapper.text()).toMatch(siteTitle);
+  });
+});
