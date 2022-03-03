2a3e346d2cdc77a85a4fd7e719778547470be1dd
Amina N Oti <Amina.Oti@ibm.com>
2022-03-03 09:26:17 +0000

    feat: add background image to banner

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -1,14 +1,13 @@
 <template>
-  <div>
-    <img v-bind:src="backgroundImage" />
+  <div class="container">
     <header>
-      <h1>hello</h1>
+      <h1 v-bind:src="backgroundImage">hello</h1>
     </header>
   </div>
 </template>
 
 <script>
-import backgroundImage from "../assets/images/BannerComponentBackground.jpeg";
+import "../assets/styles/theme-colors.scss";
 
 export default {
   name: "BannerComponent",
@@ -16,17 +15,19 @@ export default {
     bannerTitle: String,
   },
   data() {
-    return {
-      backgroundImage: backgroundImage,
-    };
+    return {};
   },
 };
 </script>
 
 <style scoped>
+.container {
+  background-image: url("../assets/images/BannerComponentBackground.jpeg");
+  height: 500px;
+}
 h1 {
   text-align: center;
-  color: white;
+  color: var(--secondary-color);
   font-size: var(--title-size);
 }
 </style>
