9949c65f562b7de4860cbdc03d57bdc914e4998e
Amina N Oti <Amina.Oti@ibm.com>
2022-03-14 09:43:42 +0000

    feat: moved image to HTML

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="container">
+    <img src="../assets/images/BannerComponentBackground.jpeg" alt="Snow" />
     <header>
-      <h1 v-bind:src="backgroundImage">hello</h1>
+      <div class="centered">Centered</div>
     </header>
   </div>
 </template>
 
 <script>
 import "../assets/styles/theme-colors.scss";
+import AddTodoItem from "./AddTodo.vue";
 
 export default {
   name: "BannerComponent",
   props: {
     bannerTitle: String,
+    AddTodoItem,
   },
   data() {
     return {};
@@ -22,12 +25,21 @@ export default {
 
 <style scoped>
 .container {
-  background-image: url("../assets/images/BannerComponentBackground.jpeg");
-  height: 500px;
+  height: 10%;
 }
 h1 {
   text-align: center;
   color: var(--secondary-color);
   font-size: var(--title-size);
+}
+.container-title {
+  margin-top: 100px;
+}
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: var(--secondary-color);
 }
 </style>
