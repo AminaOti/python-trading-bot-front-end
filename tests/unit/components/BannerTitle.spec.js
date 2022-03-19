c71652929dc4ec37887d05a14151f2b361b4ee15
Amina N Oti <Amina.Oti@ibm.com>
2022-03-19 15:53:46 +0000

    test: updated BannerTitle tests

--- tests/unit/components/BannerTitle.spec.js
+++ tests/unit/components/BannerTitle.spec.js
@@ -1,21 +1,54 @@
 import { shallowMount } from "@vue/test-utils";
-import bannerTitle from "@/components/bannerComponent/BannerTitle.vue"; //src/components/bannerComponent/BannerComponent.vue
+import { render } from "@vue/server-test-utils";
+import bannerTitleComponent from "@/components/bannerComponent/BannerTitle.vue"; //src/components/bannerComponent/BannerComponent.vue
+
+//Should receive the title and logo image from propsx
+//should render the title that was passed through props from
+//should render the image that was passed through props from
+
+const MOCK_SITE_TITLE = "Mock Site Title";
+const MOCK_BANNER_ICON = "images/testImage.jpeg";
+
+const shallowMountWrapperBannerTitle = (props) =>
+  shallowMount(bannerTitleComponent, {
+    propsData: props || {
+      bannerTitle: MOCK_SITE_TITLE,
+      bannerIcon: MOCK_BANNER_ICON,
+    },
+  });
 
 describe("The Banner Title - BannerTitle.vue", () => {
-  it("should render the title passed through the props", () => {
-    const title = "Mock Site Title";
-    const bannerIcon = "images/testImage.jpeg";
-    const wrapper = shallowMount(bannerTitle, {
-      propsData: { bannerTitle: title, bannerIcon: bannerIcon },
+  describe("should receive the title and logo image from props", () => {
+    it("should RECEIVE the title passed through the props", () => {
+      const wrapper = shallowMountWrapperBannerTitle();
+      expect(wrapper.props().bannerTitle).toBe(MOCK_SITE_TITLE);
     });
-    expect(wrapper.props().bannerTitle).toBe(title);
+
+    it("should receive the logo image passed through the props", () => {
+      const wrapper = shallowMountWrapperBannerTitle();
+      expect(wrapper.props().bannerIcon).toBe(MOCK_BANNER_ICON);
+    });
   });
-  it("should match snapshot", () => {
-    const title = "Mock Site Title";
-    const bannerIcon = "images/testImage.jpeg";
-    const wrapper = shallowMount(bannerTitle, {
-      propsData: { bannerTitle: title, bannerIcon: bannerIcon },
+
+  describe("should DISPLAY the title and image passed through the props", () => {
+    it("should display the title passed through props", () => {
+      const wrapper = shallowMountWrapperBannerTitle();
+      expect(wrapper.html()).toContain("<src");
     });
+
+    it("should display the logo image passed through props", async () => {
+      const wrapper = shallowMountWrapperBannerTitle();
+      console.log(wrapper.html());
+      //expect(wrapper.html()).toContain('<img')
+      //findAll
+      const renderedWrapper = await render(wrapper);
+      console.log(renderedWrapper);
+      expect(wrapper.findAll(MOCK_SITE_TITLE)).toContain("<img");
+    });
+  });
+
+  it("should match snapshot", () => {
+    const wrapper = shallowMountWrapperBannerTitle();
     expect(wrapper.element).toMatchSnapshot();
   });
 });
