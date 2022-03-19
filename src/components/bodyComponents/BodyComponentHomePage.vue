f2f1510e3ab28aae9950fe9e0e3d2dbd79777e21
Amina N Oti <Amina.Oti@ibm.com>
2022-03-19 18:05:14 +0000

    feat: bodyComponentHomePage & update to package.json

---
+++ src/components/bodyComponents/BodyComponentHomePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hello</h1>
+</template>
+
+<script>
+export default {
+  name: "BodyComponentHomePage",
+  props: {
+    backgroundColour: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.bodyComponentHomePage_Container {
+  background-color: var(--backgroundColour);
+}
+</style>
