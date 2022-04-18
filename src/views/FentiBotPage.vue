253798a83e1885b868c00d0c34d23c9666e82e0e
Amina N Oti <Amina.Oti@ibm.com>
2022-04-18 12:45:22 +0100

    feat: add banner to FentiBot page

--- src/views/FentiBotPage.vue
+++ src/views/FentiBotPage.vue
@@ -1,19 +1,27 @@
 <template>
   <span test-id="FentiBotPage"></span>
   <TheMasthead :siteTitle="siteTitle"></TheMasthead>
+  <BannerComponent
+    :bannerTitle="pageTitle"
+    :bannerIcon="bannerIcon"
+  ></BannerComponent>
   <h1>Fenti Bot Page</h1>
 </template>
 <script>
 import TheMasthead from "../components/TheMastHead.vue";
+import BannerComponent from "../components/bannerComponent/BannerComponent.vue";
+
 export default {
   name: "FentiBotPage",
   components: {
     TheMasthead,
+    BannerComponent,
   },
   data() {
     return {
       siteTitle: "Thicc Dough Crypto",
-      bannerIcon: "images/ThiccDoughCryptoLogo.png",
+      pageTitle: "Fenti Bot",
+      bannerIcon: "images/FentyBotLogo.png",
     };
   },
 };
