dd17f7325025cb02f6e9840630d5790ff32b5075
Amina N Oti <Amina.Oti@ibm.com>
2022-04-18 13:22:21 +0100

    fix: remove comments

--- src/components/homePageComponent/BotTileComponent.vue
+++ src/components/homePageComponent/BotTileComponent.vue
@@ -28,7 +28,6 @@ export default {
     return {};
   },
   methods: {
-    // emit the new search term to SearchBar
     navigateToBotPage() {
       if (this.botName == "Fenti") {
         this.$router.push("/FentiBot");
