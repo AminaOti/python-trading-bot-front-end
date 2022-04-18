12789b208851491b4e6b011b9a30922ce5f53333
Amina N Oti <Amina.Oti@ibm.com>
2022-04-18 11:46:43 +0100

    feat: add route to bot tile

--- tests/unit/components/BotTileComponent.spec.js
+++ tests/unit/components/BotTileComponent.spec.js
@@ -2,12 +2,23 @@ import botTileComponent from "@/components/bodyComponents/BotTileComponent.vue";
 import { shallowMount } from "@vue/test-utils";
 const MOCK_BOT_NAME = "Mock bot name";
 const MOCK_BOT_LOGO = "images/testImage.jpeg";
+const FENTI_BOT_NAME = "Fenti";
+const FENTI_BOT_ROUTE = "/FentiBot";
+
+const mockRouter = {
+  push: jest.fn(),
+};
 
 const shallowMountcomponentBotTileComponent = (props) =>
   shallowMount(botTileComponent, {
     propsData: props || {
       botName: MOCK_BOT_NAME,
       botLogo: MOCK_BOT_LOGO,
+    },
+    global: {
+      mocks: {
+        $router: mockRouter,
+      },
     },
   });
 
@@ -32,6 +43,18 @@ describe("Bot tile (child component) - BotTileComponent.vue", () => {
     //   const component = shallowMountcomponentBotTileComponent();
     //   expect(component.html()).toContain(MOCK_BOT_LOGO);
     // });
+  });
+
+  describe("should be able to naviate to the correct bot page when a tile is clicked", () => {
+    it("should navigate to the FentiBotPage when the fenti bot tile is clicked", async () => {
+      const props = {
+        botName: FENTI_BOT_NAME,
+        botLogo: MOCK_BOT_LOGO,
+      };
+      const component = shallowMountcomponentBotTileComponent(props);
+      await component.find("[data-test=bot-tile]").trigger("click");
+      expect(mockRouter.push).toHaveBeenCalledWith(FENTI_BOT_ROUTE);
+    });
   });
 
   it("should match snapshot", () => {
