39f681e3e2d9830036378598dd38309e0b01aae3
Amina N Oti <Amina.Oti@ibm.com>
2022-05-14 20:33:07 +0100

    feat: getAssetData

--- src/components/fentiPageComponents/AssetTileComponent.vue
+++ src/components/fentiPageComponents/AssetTileComponent.vue
@@ -2,6 +2,8 @@
   <div test-id="fenti-bot-tile"></div>
   <div>
     <h1>{{ assetTitle }}</h1>
+    <p>{{ assetData }}</p>
+    <p>---</p>
   </div>
 </template>
 
@@ -9,6 +11,25 @@
 export default {
   props: {
     assetTitle: String,
+  },
+  data() {
+    return {
+      assetData: [],
+    };
+  },
+  computed: {},
+  methods: {
+    async getAssetData() {
+      const url = `${process.env.VUE_APP_API_URL_FOR_FETCH_FENTI_ASSET_DATA}/${this.assetTitle}`;
+      const response = await fetch(url).catch("error");
+      console.log("hello");
+      console.log(response);
+      const responseToJson = await response.json();
+      this.assetData = responseToJson.data;
+    },
+  },
+  beforeMount() {
+    this.getAssetData();
   },
 };
 </script>
--- tests/unit/components/FentiBotAssetTile.spec.js
+++ tests/unit/components/FentiBotAssetTile.spec.js
@@ -1,6 +1,10 @@
 import AssetTileComponent from "@/components/fentiPageComponents/AssetTileComponent.vue";
 import { shallowMount } from "@vue/test-utils";
 const MOCK_ASSET_TITLE = "Polkadot";
+const MOCK_ASSET_DATA = [
+  { time: 1641945600000, "sellOrBuy?": "buy", price: 27.44 },
+  { time: 1642636800000, "sellOrBuy?": "sell", price: 24.1 },
+];
 
 const shallowMountAssetTileComponent = (props) =>
   shallowMount(AssetTileComponent, {
@@ -9,10 +13,27 @@ const shallowMountAssetTileComponent = (props) =>
     },
   });
 
-//FentiPageBodyComponent (child component) - FentiPageBodyComponent.vue
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () =>
+      Promise.resolve({
+        status: 200,
+        success: true,
+        data: MOCK_ASSET_DATA,
+      }),
+    catch: "error",
+  })
+);
+
 describe("FentiBotAssetTile (child component) - FentiBotAssetTile.vue", function () {
   it("should receive asset titles from the parent component ", () => {
     const component = shallowMountAssetTileComponent();
     expect(component.props().assetTitle).toBe(MOCK_ASSET_TITLE);
+  });
+
+  it("it should store a list of asset data fetched from the database", async () => {
+    const component = shallowMountAssetTileComponent();
+    await component.vm.getAssetData();
+    expect(component.vm.$data.assetData).toEqual(MOCK_ASSET_DATA);
   });
 });
